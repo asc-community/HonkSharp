a91066ff3563a31b20a187350f460127298c7875
WhiteBlackGoose <wbg@angouri.org>
2021-06-04 00:16:11 +0300

    Sequence operations

---
+++ DeclarativeCSharp/Fluency/Seqs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeclarativeCSharp.Fluency
+{
+    public static class SeqsExtensions
+    {
+        public static IEnumerable<(T1 left, T2 right)> Zip<T1, T2>(this (IEnumerable<T1>, IEnumerable<T2>) seqs)
+        {
+            var iterLeft = seqs.Item1.GetEnumerator();
+            var iterRight = seqs.Item2.GetEnumerator();
+            bool leftAdv, rightAdv;
+            while ((leftAdv = iterLeft.MoveNext()) & (rightAdv = iterRight.MoveNext()))
+                yield return (iterLeft.Current, iterRight.Current);
+
+            if (leftAdv != rightAdv)
+                throw new InvalidOperationException("Collections should have the same size");
+        }
+
+        public static IEnumerable<(T1 left, T2 right)> Zip<T1, T2>(this IEnumerable<T1> left, IEnumerable<T2> right)
+            => (left, right).Zip();
+
+        public static IEnumerable<(T1 left, T2 right)> Cartesian<T1, T2>(this (IEnumerable<T1>, IEnumerable<T2>) seqs)
+        {
+            foreach (var a in seqs.Item1)
+                foreach (var b in seqs.Item2)
+                    yield return (a, b);
+        }
+
+        public static IEnumerable<(T1 left, T2 right)> Cartesian<T1, T2>(this IEnumerable<T1> left, IEnumerable<T2> right)
+            => (left, right).Cartesian();
+    }
+}
