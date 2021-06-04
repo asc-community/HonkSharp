3e85860458850e68f19314d4f10a2653c974dbb8
WhiteBlackGoose <wbg@angouri.org>
2021-06-04 20:55:46 +0300

    Sample

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -5,15 +5,13 @@ using DeclarativeCSharp.Fluency;
 using DeclarativeCSharp.Functional;
 
 
-(..)
-.AsRange()
-.AssumeBest()
-.ExecuteForAll(_ => 
-    Console.ReadLine()
-    .Parse<byte>()
-    .Match(
-        b => $"Success! {b}",
-        () => "Nope :("
-    )
-    .Execute(Console.WriteLine)
-);
+5
+.LetLazy(out var big, a => a + 3 + (int)Math.Sqrt(a))
+.Inject(big)
+.Map((a, big) => a switch
+{
+    > 12 => a + big,
+    > 6 and < 10 => big * 2,
+    _ => 6
+})
+.Execute(Console.WriteLine);
