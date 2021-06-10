c4b4032e9e5e4027ef0247a17bab8be95b2c2337
WhiteBlackGoose <wbg@angouri.org>
2021-06-11 00:42:20 +0300

    Downcast of single type

--- DeclarativeCSharp/Fluency/Downcast.cs
+++ DeclarativeCSharp/Fluency/Downcast.cs
@@ -6,7 +6,11 @@ namespace DeclarativeCSharp.Fluency
 {
     public static class ConvertExtensions
     {
-        public static TTo Downcast<TFrom, TTo>(this TFrom o) where TTo : TFrom
-            => (TTo)(o ?? throw new ArgumentNullException());
+        public static TTo Downcast<TTo>(this object o)
+            => o switch
+            {
+                TTo res => res,
+                _ => throw new InvalidCastException()
+            };
     }
 }
